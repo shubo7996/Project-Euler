bf3641d16352f1d5de4920774da679ee2dc747cb
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-04-16 22:55:29 +0530

    Updated isPrime func

--- Euler_Prime.py
+++ Euler_Prime.py
@@ -1,3 +1,5 @@
+import math,random
+
 def seive(n):
 	primeList=[True]*(n+1)
 	prime_list=[]
@@ -16,25 +18,26 @@ def seive(n):
 			
 	return prime_list
 
-def isPrime(n):
-    if (n <= 1):
-    	return False
-    if (n == 2):
-    	return True
-    if (n % 2 == 0):
-    	return False
-    if (n < 9):
-    	return True
-    if (n % 3 == 0):
-    	return False
-            
-    counter = 5;            
-    while((counter * counter) <= n):
-        if (n % counter == 0):
-        	return False
-        if (n % (counter + 2) == 0):
-        	return False;
-        counter += 6
-            
-
-    return True
+def isPrime(n, k = 3):
+    if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
+        return [False, False, True, True, False, True][n]
+    elif n % 2 == 0:  # should be faster than n % 2
+        return False
+    else:
+        s, d = 0, n - 1
+        while d % 2 == 0:
+            s, d = s + 1, d >> 1
+      # A for loop with a random sample of numbers
+        for a in random.sample(range(2, n-2), k):
+            x = pow(a, d, n)
+            if x != 1 and x + 1 != n:
+                for r in range(1, s):
+                    x = pow(x, 2, n)
+                    if x == 1:
+                        return False  # composite for sure
+                    elif x == n - 1:
+                        a = 0  # so we know loop didn't continue to end
+                        break  # could be strong liar, try another a
+                if a:
+                    return False  # composite if we reached end of this loop
+        return True  # probably prime if reached end of outer loop
