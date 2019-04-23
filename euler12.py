33ea0a2a81ea27c665db20acb91ce180cab8b32c
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-04-23 16:50:38 +0530

    Improved Time

--- euler12.py
+++ euler12.py
@@ -1,3 +1,12 @@
+'''
+@Solution-Subhamoy Paul
+'''
+
+"""
+	Method 1
+
+"""
+"""
 def isTriangular(num):
 	if num<0:
 		return False
@@ -24,9 +33,34 @@ def checkFactors(num):
 		return True
 
 if __name__ == '__main__':
-	for num in range(76000000,77000000):
+	for num in range(76_000_000,77_000_000):
 		if (isTriangular(num)):
 			print('{} is Triangular with more than 500 factors'.format(num))
 		#else:
 		#	print("Not Triangular!")
+"""
 
+
+"""
+	Method 2
+"""
+
+
+from sympy import factorint
+import time
+
+start=time.perf_counter()
+
+for x in range(100,100_000_00):
+	list_sum=sum([num for num in range(x+1)])
+	vals_=1
+	fact_dict=factorint(list_sum)
+	
+	for vals in fact_dict.values():
+		vals_*=(vals+1)
+	
+	if (vals_)>500:
+		print(list_sum)
+		break
+		
+print(f"Time taken: {time.perf_counter()-start}")
