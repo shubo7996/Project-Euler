dd66b8bf336a752c4ff2fda33b1582b0ddf4a8e4
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-04-25 12:53:40 +0530

    comment_lines added to Dp Soln

--- euler81.py
+++ euler81.py
@@ -28,13 +28,13 @@ print(f"Shape: {final_matrix.shape}")
 matrix_size=final_matrix.shape[0]
 #print(matrix_size)
 for i in range(matrix_size-2,-1,-1):
-    final_matrix[matrix_size-1,i]+=final_matrix[matrix_size-1,i+1]
-    final_matrix[i,matrix_size-1]+=final_matrix[i+1,matrix_size-1]
+    final_matrix[matrix_size-1,i]+=final_matrix[matrix_size-1,i+1]#Adding Number to the Right
+    final_matrix[i,matrix_size-1]+=final_matrix[i+1,matrix_size-1]#Adding Number to the Bottom
 
 for row in range(matrix_size-2,-1,-1):
     for col in range(matrix_size-2,-1,-1):
         #current_val=final_matrix[row,col]
-        final_matrix[row,col]+=min(final_matrix[row+1,col],final_matrix[row,col+1])
+        final_matrix[row,col]+=min(final_matrix[row+1,col],final_matrix[row,col+1])#current_val+min(right,bottom)
 
 print(f"Resultant Matrix:\n {final_matrix}")
 print(f"Minimal Sum Path: {final_matrix[0,0]}")
