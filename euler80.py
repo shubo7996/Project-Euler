53d9528d5727d9d1d611599f781bca2708b48e10
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-04-26 14:46:39 +0530

    Issue Fixed

--- euler80.py
+++ euler80.py
@@ -1,16 +1,21 @@
-"""
-#This Solution calculates precision upto just 100 decimal places 
+'''
+#This Solution calculates precision upto just 100 decimal places
 
+Solution- @Subhamoy Paul 
+
+'''
 import math
-#from fractions import Fraction
 import decimal 
+import time
+
+start=time.perf_counter()
 
 def compute_precision(x):
     '''
         Computes decimal precison upto 100
     '''
     with decimal.localcontext() as ctx:
-        ctx.prec=101
+        ctx.prec=102
         division= decimal.Decimal(x).sqrt()
     return division
     
@@ -19,49 +24,12 @@ sum_=0
 for x in range(1,101):
     #check if sq root of a number is rational or not
     if (math.sqrt(x)-int(math.sqrt(x))):
-        #convert the sq root into fraction form
-        #fract_tup=Fraction(math.sqrt(x))
-        #numerator and denominator of the above resultant fraction passed as parameters
-        #dec_=compute_precision(fract_tup.numerator,fract_tup.denominator,100)
         dec_=compute_precision(x)
-        #converting the decimal value into an array of int and decimal
         arr=str(dec_).split('.')
-        #print(x,"------>",len(arr[1]))
-        sum_+=sum(list(map(int,list(arr[1]))))
+        #new_arr2_sum=arr[1][1:99]
+        #print(x,"---->",sum(map(int,list(new_arr2_sum))))
+        #print(x,"------>",sum(list(map(int,list(arr[0]+arr[1])))))
+        sum_+=sum(list(map(int,list(arr[0]+arr[1][0:99]))))
 
-print(sum_)
-
-"""
-
-
-    #Using Square root by Subtraction
-
-
-import math
-
-def square_root(num,prec_limit):
-    limit=math.pow(10,prec_limit+1)
-    a,b=5*num,5
-
-    while(b<limit):
-        if a>b:
-            a-=b
-            b+=10
-        else:
-            a*=100
-            b=(b//10)*100+5
-
-    return b//100
-
-def main():
-    sum_=0
-    precision=100
-    for x in range(1,101):
-        if (math.sqrt(x)-int(math.sqrt(x))):
-            #y+=1
-            #continue
-            sum_+=sum(list(map(int,list(str(square_root(x,precision))))))
-    print(sum_)
-
-if __name__=='__main__':
-    main()
+print("Result is:",sum_)
+print(f"Time Elapsed: {time.perf_counter()-start}")
