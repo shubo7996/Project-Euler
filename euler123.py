5cf6776a9bb4df65472eee9972ecdc3b5f95430a
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-05-24 17:01:51 +0530

    Mathematica Soln coming soon

---
+++ euler123.py
@@ -0,0 +1,18 @@
+from Euler_Prime import seive
+
+prime_list=seive(400000)
+#(pn−1)^n + (pn+1)^n is divided by pn^2.
+for x in range(7038,len(prime_list)+1):
+ 	pn=prime_list[x]
+ 	#print(pn)
+ 	# if ((pow(pn-1,x+2)+pow(pn+1,x+2))%(pow(pn,2))>=10**10): #computationally slow
+ 	# 	print(x)
+ 	# 	break
+ 	if 2*pn*(x)>pow(10,10):
+ 		print(x+2)
+ 		break
+
+
+
+
+
