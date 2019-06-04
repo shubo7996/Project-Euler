5091d1ee88dfbe0fa668b3adc14d7372c3d90db3
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-06-04 14:10:09 +0530

    Used dict instead of list

---
+++ Python/euler62.py
@@ -0,0 +1,24 @@
+import time
+
+def main():
+    cubes={}
+    i=0
+    while True:
+        cube=sorted(list(str(i**3)))
+        if i not in cubes.keys():
+            try:
+                cubes[i]=cube
+            except:
+                print("Exception Occured!")
+
+        if list(cubes.values()).count(cube)==5:
+            result=(list(cubes.keys())[list(cubes.values()).index(cube)])
+            break
+        i+=1
+    print(result**3)
+    print(cubes)
+
+if __name__ == '__main__':
+    start=time.perf_counter()
+    main()
+    print(f"Time Elapsed:{time.perf_counter()-start}")
