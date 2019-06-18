dff7e5b4cc79804b2d81b635ace0fed291e83607
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-06-19 00:58:44 +0530

    Error Fixed Mst Wip

--- Python/euler107.py
+++ Python/euler107.py
@@ -4,13 +4,13 @@ class Vertex(object):
 		self.id=node
 		self.adjacent={}
 
-	def __str(self):
-		return str(self.id) + 'adjacent:' + str([x.id for x in self.adjacent])
+	def __str__(self):
+		return str([x.id for x in self.adjacent])
 
 	def add_neighbours(self,neighbour,weight):
 		self.adjacent[neighbour]=weight
 
-	def get_connection(self):
+	def get_connections(self):
 		return self.adjacent.keys()
 
 	def get_id(self):
@@ -28,27 +28,25 @@ class Graph(object):
 		self.__graph_dict = graph_dict
 		self.num_vertices=0
 
-
 	def __iter__(self):
 		return iter(self.__graph_dict.values())
 
 	def get_vertices(self):
 		return list(self.__graph_dict.keys())
 
-
 	def add_vertex(self,node):
 		self.num_vertices+=1
 		new_vertex=Vertex(node)
 		#if node not in self.__graph_dict:
 		#	self.__graph_dict[node]=[new_vertex]
-		self.graph_dict[node]=new_vertex
+		self.__graph_dict[node]=new_vertex
 		return new_vertex
 
 	def get_vertex(self,n):
 		if n in self.__graph_dict:
 			return self.__graph_dict[n]
 		else:
-			returnn None
+			return None
 
 	def add_edge(self,start,end,cost=0):
 		if start not in self.__graph_dict:
@@ -67,13 +65,6 @@ class Graph(object):
 	# def edges(self):
 	# 	return self.__generate_edges()
 
-	# def __generate_edges(self):
-	# 	edges=[]
-	# 	for vertex in self.__graph_dict:
-	# 		for neighbour in self.__graph_dict[vertex]:
-	# 			if {neighbour,vertex} not in edges:
-	# 				edges.append({vertex,neighbour})
-	# 	return edges
 
 
 if __name__ == '__main__':
@@ -90,7 +81,7 @@ if __name__ == '__main__':
 	graph_obj.add_edge("B","D",17)
 	graph_obj.add_edge("B","E",20)
 	graph_obj.add_edge("C","D",28)
-	graph_obj.add_edge("C","E",31)
+	graph_obj.add_edge("C","F",31)
 	graph_obj.add_edge("D","E",18)
 	graph_obj.add_edge("D","F",19)
 	graph_obj.add_edge("D","G",23)
@@ -98,15 +89,13 @@ if __name__ == '__main__':
 	graph_obj.add_edge("F","G",27)
 
 
-	
+	for v in graph_obj:
+		for w in v.get_connections():
+			vID=v.get_id()
+			wID=w.get_id()
+			print(f"{vID} " + "---> " + f"{wID} " + "has weight "+ f"{v.get_weight(w)} ")
 
 
-
-	#print("Vertices of the Graph: ")
-	#print(graph_obj.vertices())
-
-	#print("Edges of the Graph: ")
-	#print(graph_obj.edges())
-
-	print(graph_obj)
+	for v in graph_obj:
+		print(f"__graph_dict[{v.get_id()}]" + "= " + f"{v}")
 
