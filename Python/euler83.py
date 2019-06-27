53299f98e34cceba299ca4bc103f30c57078144f
Subhamoy Paul <whatsupmofo420@gmail.com>
2019-06-27 22:30:06 +0530

    BFS Minimmal

--- Python/euler83.py
+++ Python/euler83.py
@@ -1,63 +1,78 @@
-from numpy import matrix
+import numpy as np
 
-def readFile(filepath):
-	matrix_ = list()
-	with open(filepath, 'r') as filehandle:  
-		for line in filehandle:  
-			currentPlace = line.split('\n')
-			res=list(currentPlace[0].split(','))
-			final_res=list(map(int,res))
-			matrix_.append(final_res)
+'''
+	Read File into a 2D Matrix
+'''
+filepath='C:\\Users\\kiit1\\Documents\\Codes\\Project-Euler\\euler83.txt'
+matrix_ = list()
+with open(filepath, 'r') as filehandle:  
+	for line in filehandle:  
+		currentPlace = line.split('\n')
+		res=list(currentPlace[0].split(','))
+		final_res=list(map(int,res))
+		matrix_.append(final_res)
 
-	return matrix(matrix_)
 
-def find_minimal_path_using_bfs(matrix):
-	minimal_sum=0
-	visited={}	
-	queue=[]
-	row_,col_=0,0
-	queue.append(matrix[0,0])
-	visited[matrix[0,0]]=True
-	des=matrix[79,79]
+matrix=np.matrix(matrix_)
 
-	while(queue):
-		
-		if col_>matrix.shape[0]-1:
-			col_=0
-			row_+=1
-		
-		element=queue.pop(0)
-		minimal_sum+=element
-		visited[element]=True
+''' Global Variables '''
 
-		if col_==0:
-			queue.extend([matrix[row_,col_+1],matrix[row_+1,col_]])
-			current_min=min(matrix[row_,col_+1],matrix[row_+1,col_])
-			minimal_sum+=current_min
-		elif col==matrix.shape[0]-1:
-			queue.extend([matrix[row_,col_-1],matrix[row_+1,col_]])
-			current_min=min(matrix[row_,col_-1],matrix[row_+1,col_])
-			minimal_sum+=current_min
-		else:
-			queue.extend([matrix[row_,col_+1],matrix[row_+1,col_],matrix[row_-1,col_]])
-			current_min=min(matrix[row_,col_+1],matrix[row_+1,col_],matrix[row_-1,col_])
-			minimal_sum+=current_min
-		current_position=[(ix,iy) for ix, row in enumerate(matrix) for iy, i in enumerate(row) if current_min]
-		row_,col_=current_position[0]
+ROW,COL=matrix.shape[0],matrix.shape[1]
+visited=np.matrix([[False]*COL]*ROW)	
+row_queue,col_queue=[],[]
+row_,col_=0,0
+reached_dest=False
+#minimal_sum=matrix[row_,col_]
+move_count=0
+nodes_left_in_layer=1
+nodes_in_next_layer=0
 
-		if visited[des]==True:
+direction_row=[-1,+1,0,0]
+direction_col=[0,0,+1,-1]
+	
+
+def find_minimal_path_using_bfs():
+	row_queue.append(row_)
+	col_queue.append(col_)
+	visited[row_,col_]=True
+	while len(row_queue)>0:
+		r=row_queue.pop(0)
+		c=col_queue.pop(0)
+		if matrix[r,c]==matrix[ROW-1,COL-1]:
 			break
+		explore_neighbours(r,c)
+		nodes_left_in_layer-=1
+		if nodes_left_in_layer==0:
+			nodes_left_in_layer=nodes_in_next_layer
+			nodes_in_next_layer=0
+			move_count+=1
+	if reached_dest:
+		return move_count
+	return-1
 
-	return minimal_sum
+def explore_neighbours(r,c):
+	for x in range(0,4):
+		rr=r+direction_row[x]
+		cc=c+direction_col[x]
+
+		#skipping out of bound location
+		if rr<0 or cc<0:
+			continue
+		if rr>=ROW or cc>=COL:
+			continue
+		#Skipping visited location
+		if visited[rr,cc]:
+			continue
+
+		row_queue.append(rr)
+		col_queue.append(cc)
+		visited[rr,cc]=True
+		nodes_in_next_layer+=1
 
 
-def main():
-	file_path='C:\\Users\\kiit1\\Documents\\Codes\\Project-Euler\\euler83.txt'
-	adj=readFile(file_path)
-	result=find_minimal_path_using_bfs(adj)
-	print(result)
+def solve():
+	find_minimal_path_using_bfs()
 
 if __name__ == '__main__':
-	main()
-
+	solve()
 
